477ec3d8f7b8a13cf23658b13c29a7df0683f84c
Chico Almeida <francisco.almeida@outlook.com>
2016-10-10 09:25:14 -0300

    create base booking service

---
+++ src/main/java/com/brownfield/booking/BrownFieldBookingApplication.java
@@ -0,0 +1,12 @@
+package com.brownfield.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownFieldBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrownFieldBookingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brownfield/booking/BrownFieldBookingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brownfield.booking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrownFieldBookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
